ecbf417429c69fdab53b9290ab249778b2d8d8bf
Brandon_ <129625854+brandonthis-that@users.noreply.github.com>
2024-01-25 22:44:38 +0300

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,14 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 999
+vim.opt.inccommand = "split" --use :%s/to look for words to edit
+vim.opt.ignorecase = true
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "

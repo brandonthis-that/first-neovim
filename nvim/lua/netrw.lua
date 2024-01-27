eb3916bfab687bbeccdc17f2260e9a03d567092b
Brandon_ <129625854+brandonthis-that@users.noreply.github.com>
2024-01-27 15:16:32 +0300

    Update netrw.lua

--- nvim/lua/netrw.lua
+++ nvim/lua/netrw.lua
@@ -1,15 +1,11 @@
-
-
 vim.cmd [[
   augroup AutoOpenNetrw
     autocmd VimEnter * :Lexplore <split> winheight=10 winwidth=20
   augroup END
 ]]
 
-
 vim.g.netrw_altv          = 1
-vim.g.netrw_fastbrowse    = 2
-vim.g.netrw_keepdir       = 0
+vim.g.netrw_localrmdir    = 1  -- Add this line
 vim.g.netrw_liststyle     = 3
 vim.g.netrw_retmap        = 1
 vim.g.netrw_silent        = 1
@@ -18,20 +14,10 @@ vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 4
 vim.g.netrw_winsize = 20
 
-
-
 function ToggleNetrwFocus()
-    local winnr = vim.fn.winnr()
-    local bufname = vim.fn.bufname()
-    for i = 1, vim.fn.winnr('$') do
-        if vim.fn.winbufnr(i) == bufname and i ~= winnr then
-            vim.cmd(i .. 'wincmd w')
-            return
-        end
-    end
-    vim.cmd('Lexplore')
+    -- ... (existing code)
 end
 
-
-vim.api.nvim_set_keymap("n", "<leader>e", ":lua ToggleNetrwFocus()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>e", ":Lexplore<CR>", { noremap = true })  -- Added keymap
 vim.api.nvim_set_keymap("n", "<leader>m", "<C-w>w", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>u", ":ExploreUp<CR>", { noremap = true })  -- Add this line
